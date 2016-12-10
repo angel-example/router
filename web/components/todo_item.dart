487aee770dc1766e343ffd1c0c3e2300a22215f2
thosakwe <thosakwe@gmail.com>
2016-12-09 22:44:35 -0500

    Fixed

--- web/components/todo_item.dart
+++ web/components/todo_item.dart
@@ -3,6 +3,7 @@ import '../todo.dart';
 
 Element renderTodo(Todo todo) {
   return new Element.html(
-      '''<a class="todo-item ${todo.completed ? 'complete' : 'incomplete'}"
-          href="#/todos/${todo.id}">${todo.text}</a>''');
+      '''<li class="todo-item ${todo.completed ? 'complete' : 'incomplete'}">
+          <a href="#/todos/${todo.id}">${todo.text}</a>
+        </li>''');
 }
--- web/components/todo_list.dart
+++ web/components/todo_list.dart
@@ -7,19 +7,27 @@ import 'todo_item.dart';
 Component todoList() => new TodoListComponent();
 
 class TodoListComponent extends Component {
+  TodoRepository _todos;
+
   @override
   Element render(RoutingResult result, Injector injected) {
     document.title = 'Todos';
 
-    final TodoRepository repo = injected.get(TodoRepository);
-    final root = new Element.html(
-        '''<div class="content todo-list">
-            <h1>Todos (${repo.todos.length})</h1>
+    _todos = injected.get(TodoRepository);
+    final root = new Element.html('''<div class="content todo-list">
+            <h1>Todos (${_todos.todos.length})</h1>
             <br>
             <a href="#/todos/new">Add</a>
             <br>
+            <ul></ul>
             <br>
           </div>''');
-    return root..children.addAll(repo.todos.map(renderTodo));
+    return root;
+  }
+
+  @override
+  void afterRender(Element $root) {
+    final UListElement $ul = $root.querySelector('ul');
+    $ul.children.addAll(_todos.todos.map(renderTodo));
   }
 }
